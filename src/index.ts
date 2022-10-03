ac02f684d5f35692e7467bcd7922c3e4647efef0
Andrey Klochok <andreyklochok@gmail.com>
2022-10-03 09:37:57 +0300

    refactored-demo

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,27 @@
-import Card from "./Model/Card";
+import Board from "./Board";
+import Card from "./Card";
+import Direction from "./Direction";
+import Field from "./Field";
+import Game from "./Game";
+import Generator from "./Generator";
+import Home from "./Home";
+import Pile from "./Pile";
+import Player from "./Player";
+import Row from "./Row";
+import Star from "./Star";
+import Demo from "./Demo";
 
-export { Card };
+export {
+  Board,
+  Card,
+  Direction,
+  Field,
+  Game,
+  Generator,
+  Home,
+  Pile,
+  Player,
+  Row,
+  Star,
+  Demo,
+};
